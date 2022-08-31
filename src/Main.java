f16d071e33c4e7eaa5f3d63a1d96b316f56e0b09
erdagli <3rd1fbsl4shTr>
2022-08-31 13:01:02 +0300

    Command first commit for sout

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Chain of Responsibility\n\n");
+        System.out.println("Command\n\n");
 
-        System.out.println("DocumentHandler Example:");
-        DocumentHandler documentHandler = new SpreadsheetHandler(new SlideshowHandler(new TextDocumentHandler(null)));
-
-        documentHandler.openDocument("ppt");
-        documentHandler.openDocument("txt");
-
-        System.out.println("\nIssueHandle Example:");
-        IssueHandler issueHandler = new ProductManager(new SeniorDeveloper(new JuniorDeveloper(null)));
-
-        issueHandler.fixIssue("mid");
-        issueHandler.fixIssue("low");
     }
 }
