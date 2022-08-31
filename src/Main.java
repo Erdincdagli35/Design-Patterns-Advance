61444413845da8accc2cbf95297d83adc8e0d138
erdagli <3rd1fbsl4shTr>
2022-08-31 13:02:29 +0300

    master branch updated

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Chain of Responsibility\n\n");
-
-        System.out.println("DocumentHandler Example:");
-        DocumentHandler documentHandler = new SpreadsheetHandler(new SlideshowHandler(new TextDocumentHandler(null)));
-
-        documentHandler.openDocument("ppt");
-        documentHandler.openDocument("txt");
-
-        System.out.println("\nIssueHandle Example:");
-        IssueHandler issueHandler = new ProductManager(new SeniorDeveloper(new JuniorDeveloper(null)));
-
-        issueHandler.fixIssue("mid");
-        issueHandler.fixIssue("low");
+        System.out.println("master branch");
     }
 }
