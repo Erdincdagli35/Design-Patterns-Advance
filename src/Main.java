934b988fd2ff93b3378a30b7ed0f14a2696c5c3b
erdagli <3rd1fbsl4shTr>
2022-09-05 16:49:24 +0300

    Memento created

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import text.TextDocument;
+
 public class Main {
 
+    final private static TextDocument textDocument = new TextDocument();
+    final private static Adder adder = new Adder();
+
     public static void main(String[] args) {
-        System.out.println("master branch");
+        System.out.println("text.Memento \n\n");
+
+        textDocument.write("Hello");
+        textDocument.save();
+        textDocument.print();
+
+        textDocument.write(" World");
+        textDocument.print();
+        textDocument.undo();
+        textDocument.print();
+
+        textDocument.write(" Erdinç");
+        textDocument.print();
+        textDocument.save();
+
+        textDocument.write(" Dağlı");
+        textDocument.print();
+        textDocument.undo();
+        textDocument.print();
+
+        System.out.println("text.Adder \n\n");
+
+        adder.save();
+        adder.add(10);
+        System.out.println(adder.getResult());
+
+        adder.undo();
+        System.out.println(adder.getResult());
     }
 }
